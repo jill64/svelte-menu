d11563f05232a30f0aecb83a19ad83cc6a2d909f
jill64 <intents.turrets0h@icloud.com>
2023-11-26 12:35:37 +0900

    def lib

---
+++ src/lib/Menu.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+  import { customAlphabet } from 'nanoid'
+
+  export let Class = ''
+  export let style = ''
+  export let summaryClass = ''
+  export let summaryStyle = ''
+  export let initialOpened = false
+  export let duration = 400
+  export let noOuterClosing = false
+  export let hoverOpen = false
+
+  export let onOpen: ((close: () => Promise<void>) => unknown) | undefined =
+    undefined
+
+  export let onClose: (() => unknown) | undefined = undefined
+
+  let state: 'OPENING' | 'OPENED' | 'CLOSING' | 'CLOSED' = initialOpened
+    ? 'OPENED'
+    : 'CLOSED'
+
+  let controller: AbortController | undefined = undefined
+
+  $: close = async () => {
+    if (state === 'CLOSED' || state === 'CLOSING') {
+      return
+    }
+
+    state = 'CLOSING'
+
+    controller?.abort()
+    controller = new AbortController()
+
+    onClose?.()
+
+    const timerID = setTimeout(() => {
+      state = 'CLOSED'
+      controller = undefined
+    }, duration)
+
+    controller.signal.onabort = () => clearTimeout(timerID)
+  }
+
+  $: open = async () => {
+    if (state === 'OPENED' || state === 'OPENING') {
+      return
+    }
+
+    state = 'OPENING'
+
+    controller?.abort()
+    controller = new AbortController()
+
+    onOpen?.(close)
+
+    const timerID = setTimeout(() => {
+      state = 'OPENED'
+      controller = undefined
+    }, duration)
+
+    controller.signal.onabort = () => clearTimeout(timerID)
+  }
+
+  let initialized = false
+
+  $: if (!initialized && initialOpened && close) {
+    onOpen?.(close)
+    initialized = true
+  }
+
+  $: toggle = state === 'OPENED' || state === 'OPENING' ? close : open
+
+  const id = customAlphabet(
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-'
+  )(8)
+
+  $: onClick = ({ target }: MouseEvent) => {
+    if (!noOuterClosing && !(target as Element).closest(`#${id}`)) {
+      close()
+    }
+  }
+
+  $: enter = () => {
+    if (hoverOpen) {
+      open()
+    }
+  }
+
+  $: leave = () => {
+    if (hoverOpen) {
+      close()
+    }
+  }
+</script>
+
+<svelte:window on:click={onClick} />
+
+<details
+  {id}
+  open={state !== 'CLOSED'}
+  on:mouseenter={enter}
+  on:mouseleave={leave}
+  class={Class}
+  {style}
+>
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <summary
+    style={summaryStyle}
+    class={summaryClass}
+    on:click|preventDefault={toggle}
+  >
+    <slot {state} />
+  </summary>
+  {#if state === 'OPENED' || state === 'OPENING'}
+    <slot name="contents" {close} />
+  {/if}
+</details>
+
+<style>
+  details {
+    position: relative;
+  }
+  summary {
+    cursor: pointer;
+  }
+  summary::-webkit-details-marker {
+    display: none;
+  }
+</style>
